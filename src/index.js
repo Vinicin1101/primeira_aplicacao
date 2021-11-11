78fc117b6c6c0b6897147633f205ed51f8cc8d7f
Vinicin1101 <viniciusserafim45@gmail.com>
2021-11-11 10:15:00 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Import React
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Import CSS
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Header() {
+    return(
+        <header>
+            Titulo da Pagina
+        </header>
+    )
+}
+
+ReactDom.render(
+    <React.StrictMode>
+        <Header />
+    </React.StrictMode>
+    
+    ,document.getElementById('root')
+)
